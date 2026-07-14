c41a8ce94e7d621080b9341bcc851ecd5ff8a941
Seu Nome <natalia.branco@alumni.usp.br>
2026-07-14 15:49:54 -0300

    Correção exercício 39

--- guanabara-exercicios/ex039.py
+++ guanabara-exercicios/ex039.py
@@ -3,13 +3,16 @@
 #Se é a hora de se alistar
 #Se já passou do tempo de alistamento
 #Seu programa também deverá mostrar o tempo que falta ou o que passou do prazo
-
+from datetime import date
+atual = date.today().year
 ano = int(input('Digite o ano de nascimento: '))
-idade = 2026 - ano
+idade = atual - ano
 
 if idade > 18:
-    print('Já passou {} ano(s) do tempo de alistamento!'.format(idade - 18))
+    print('Você tem {} anos! Já passou {} ano(s) do tempo de alistamento!'.format(idade, idade - 18))
+    print('Seu alistamento foi em {}.'.format(ano + 18))
 elif idade == 18:
     print('Você tem {} anos! Está na hora de se alistar!'.format(idade))
 else:
-    print('Em {} ano(s), você deve se alistar!'.format(18 - idade))
+    print('Você tem {} anos! Em {} ano(s), você deve se alistar!'.format(idade, 18 - idade))
+    print('O seu alistamento será em {}.'.format(ano + 18))

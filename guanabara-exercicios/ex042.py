8e4e4232c56d35e8635850638409a1f71b3483ad
Seu Nome <natalia.branco@alumni.usp.br>
2026-07-16 21:33:44 -0300

    Subindo alterações do exercício 42

--- guanabara-exercicios/ex042.py
+++ guanabara-exercicios/ex042.py
@@ -3,18 +3,19 @@
 #Isósceles: doislados iguais
 #Escaleno: todos os lados diferentes
 
-print('Vamos ver se forma triângulo!')
+print('Vamos ver se forma triângulo?')
+
 r1 = float(input('Escolha o tamanho do primeiro lado: '))
 r2 = float(input('Escolha o tamanho do segundo lado: '))
 r3 = float(input('Escolha o tamanho do terceiro lado: '))
 
-if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Sim, os segmentos acima podem formar triângulo!')
-    if r1 == r2 and r2 == r3:
-        print('Triângulo equilátero (todos os lados iguais)')
-    elif r1 == r2 and r2 == r3 and r3 == r1:
-        print('Triângulo isósceles (dois lados iguais)')
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:  #vamos usar condições aninhadas
+    print('Sim, os segmentos acima podem formar um triângulo ', end='') #para juntar com os textos de tipo de triângulo
+    if r1 == r2 == r3:           #eu tinha feito assim, também está certo -> r1 == r2 and r2 == r3:
+        print('equilátero (todos os lados iguais)!')
+    elif r1 != r2 != r3 != r1:   #aqui é importante fazer a comparação com r1 (exemplo digitando: 4 2 4)
+        print('escaleno (todos os lados diferentes)!')
     else:
-        print('Triângulo escaleno (todos os lados diferentes)')
+        print('isósceles (todos os lados iguais)!')
 else:
-    print('Não, os segmentos acima não podem formar triângulo!')
+    print('Não, os segmentos acima não podem formar um triângulo!')

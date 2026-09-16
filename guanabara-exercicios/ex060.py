3b8b3ee88beb959cd6bd5a5d401bc591c06de6f9
Seu Nome <natalia.branco@alumni.usp.br>
2026-09-15 22:56:05 -0300

    Subindo ex 60

--- guanabara-exercicios/ex060.py
+++ guanabara-exercicios/ex060.py
@@ -1,7 +1,8 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # Ex.: 5! = 5x4x3x2x1 = 120
 
-n = int(input('Digite um valor inteiro: '))
+#Minha resolução
+'''n = int(input('Digite um valor para calcular o fatorial: '))
 c = n
 f = n
 t = str(n) + '! = '
@@ -10,4 +11,22 @@ while c > 1:
     f = f * c
     t = t + str(c) + ' x '
 print(t)
-print(f)
+print(f)'''
+
+#Resolução simples guanabara
+'''from math import factorial
+n = int(input('Digite um valor para calcular o fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}.'.format(n, f))'''
+
+#Resolução usando o while guanabara
+n = int(input('Digite um valor para calcular o fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))

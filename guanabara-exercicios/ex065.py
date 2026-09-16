ae4e0d432dc0421c945141a6567a1e5c1a98bd3e
Seu Nome <natalia.branco@alumni.usp.br>
2026-09-15 22:58:34 -0300

    Subindo ex 65

--- guanabara-exercicios/ex065.py
+++ guanabara-exercicios/ex065.py
@@ -1,3 +1,21 @@
 #Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a
 # média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve
 # perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+resp = 'S'
+media = qtde = soma = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    qtde += 1
+    if qtde == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / qtde
+print('Você digitou {} números e a média foi {}'.format(qtde, media))
+print('O maior valor foi {} e o menor valor foi {}'.format(maior, menor))
